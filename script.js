f68184cc4e99373f9a51485391f404cfd9dec19b
hythmsam538-ux <hythmsam538@gmail.com>
2025-11-13 20:53:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,38 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 */
+
+ 
+  const btn = document.getElementById("backToTop");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  btn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // سلاسة التمرير لأي رابط داخلي
+  document.addEventListener('click', function(e) {
+    const link = e.target.closest('a[href^="#"]');
+    if (!link) return;
+    const targetId = link.getAttribute('href').substring(1);
+    const targetEl = document.getElementById(targetId);
+    if (targetEl) {
+      e.preventDefault();
+      const offset = 90; // لو عندك هيدر ثابت
+      const y = targetEl.getBoundingClientRect().top + window.pageYOffset - offset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  });
+
+   AOS.init({
+    duration: 1000, // مدة الحركة (1 ثانية)
+    once: true,     // تظهر مرة واحدة فقط عند التمرير
+    offset: 100     // تبدأ الحركة قبل الدخول بـ100 بكسل
+  });
