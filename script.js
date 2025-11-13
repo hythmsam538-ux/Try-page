6350decf934325603aa97821d96888ca18931366
hythmsam538-ux <hythmsam538@gmail.com>
2025-11-13 18:23:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,18 @@ modal.addEventListener('click', e => {
     modal.style.display = 'none';
   }
 });
+/* اراء
+document.addEventListener("DOMContentLoaded", function() {
+  const slider = document.querySelector('.slider');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+
+  nextBtn.addEventListener('click', () => {
+    slider.scrollBy({ left: 240, behavior: 'smooth' });
+  });
+
+  prevBtn.addEventListener('click', () => {
+    slider.scrollBy({ left: -240, behavior: 'smooth' });
+  });
+});
+*/
